82531deae0eef150fa800df50605a6361935bceb
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-02-05 00:09:51 +0900

    make auto calc when in tna

---
+++ src/main/java/com/github/soramame0256/showmemydps/EventListener.java
@@ -0,0 +1,114 @@
+package com.github.soramame0256.showmemydps;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.time.Instant;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EventListener {
+    private static final Pattern damageReg = Pattern.compile("-(?<damage>\\d+) .");
+    private static final String colorReg = "(§[0-9a-fik-or])";
+    private final Map<String, Integer> damageList = new HashMap<>();
+    private final Map<String, Instant> expire = new HashMap<>();
+    private Instant start = Instant.now();
+    private static final int EXPIRE_WHEN = 5000;
+    private long damage = 0;
+    private int hitCount = 0;
+    @SubscribeEvent
+    public void onEntityRender(RenderLivingEvent.Pre<EntityArmorStand> e){
+
+    }
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent e){
+        checkExpires();
+        if(Minecraft.getMinecraft().world == null) return;
+        for(Entity en : Minecraft.getMinecraft().world.getEntities(EntityArmorStand.class, (ent) -> ent.getDisplayName().getUnformattedText().contains("-"))) {
+            String name = en.getDisplayName().getUnformattedText();
+            name = name.replaceAll(colorReg, "");
+            int c = 0;
+            Matcher m = damageReg.matcher(name);
+            if (m.find()) {
+                for (String s : name.split(" ")) {
+                    if (s.startsWith("-")) {
+                        try {
+                            c += Integer.parseInt(s.replace("-", ""));
+                        } catch (NumberFormatException ex) {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (c != 0) addDamage(en.getUniqueID().toString(), c);
+        }
+    }
+    private void addDamage(String uuid, Integer da){
+        damageList.put(uuid, Math.max(damageList.getOrDefault(uuid, 0),da));
+        expire.put(uuid, Instant.now());
+    }
+    @SubscribeEvent
+    public void onChatReceive(ClientChatReceivedEvent e){
+        String s = e.getMessage().getUnformattedText().replaceAll(colorReg,"");
+        //the nameless anomaly boss prepare room lore.
+        if(s.contains("And what is responsible? What remains here should not- trapped far from whence it came. What feeds the paradox")){
+            reset();
+        }
+        if(s.contains("[!] The Raid Reward chest has been unlocked.")){
+            ShowCommand.showMsg(Minecraft.getMinecraft().player);
+        }
+
+    }
+    private void checkExpires(){
+        if(expire.size() == 0) return;
+        long i = Instant.now().toEpochMilli();
+        List<String> toRemove = new ArrayList<>();
+        for(Map.Entry<String, Instant> ep : expire.entrySet()){
+            if(ep.getValue().toEpochMilli() + EXPIRE_WHEN <= i){
+                damage += damageList.get(ep.getKey());
+                hitCount++;
+                toRemove.add(ep.getKey());
+            }
+        }
+        for(String r : toRemove){
+            expire.remove(r);
+            damageList.remove(r);
+        }
+    }
+    private void makeAllExpire(){
+        if(expire.size() == 0) return;
+        List<String> toRemove = new ArrayList<>(damageList.keySet());
+        for(String r : toRemove){
+            damage += damageList.get(r);
+            hitCount++;
+            expire.remove(r);
+            damageList.remove(r);
+        }
+    }
+    public void reset(){
+        expire.clear();
+        damageList.clear();
+        damage = 0;
+        hitCount = 0;
+        start = Instant.now();
+    }
+    public long getTotalDamage(){
+        makeAllExpire();
+        return damage;
+
+    }
+    public Instant getStartWhen(){
+        return start;
+    }
+    public int getHitCount(){
+        makeAllExpire();
+        return hitCount;
+    }
+}
---
+++ src/main/java/com/github/soramame0256/showmemydps/ShowCommand.java
@@ -0,0 +1,48 @@
+package com.github.soramame0256.showmemydps;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+import java.time.Instant;
+
+public class ShowCommand extends CommandBase {
+    @Override
+    public String getName() {
+        return "showdps";
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/showdps";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if(args.length >= 1){
+            if(args[0].equalsIgnoreCase("reset")){
+                showMsg(sender);
+                ShowMeMyDps.EVENT_LISTENER.reset();
+            }
+        }else{
+            showMsg(sender);
+        }
+    }
+    public static void showMsg(ICommandSender sender){
+        long totalDamage = ShowMeMyDps.EVENT_LISTENER.getTotalDamage();
+        int hitCount = ShowMeMyDps.EVENT_LISTENER.getHitCount();
+        if(hitCount == 0) hitCount++;
+        Instant instant = ShowMeMyDps.EVENT_LISTENER.getStartWhen();
+        long in = Instant.now().getEpochSecond() - instant.getEpochSecond();
+        sender.sendMessage(new TextComponentString("§bDPS: " + totalDamage/in));
+        sender.sendMessage(new TextComponentString("§bAvg/hit: " + totalDamage/hitCount));
+        sender.sendMessage(new TextComponentString("§bTotal: " + totalDamage));
+    }
+}
