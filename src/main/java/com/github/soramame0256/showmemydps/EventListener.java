a71a9c66b1b13265ceab3e8fecc6834dbb7b3921
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-06-13 23:41:03 +0900

    optimization, make battle time in greg accurate (WIP)

--- src/main/java/com/github/soramame0256/showmemydps/EventListener.java
+++ src/main/java/com/github/soramame0256/showmemydps/EventListener.java
@@ -3,20 +3,22 @@ package com.github.soramame0256.showmemydps;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -31,17 +33,28 @@ public class EventListener {
     private Instant start = Instant.now();
     private static final int EXPIRE_WHEN = 5000;
     private static final int DPS_AVERAGE_EXPIRE_WHEN = 1000;
-    private long damage = 0;
+    private long damage = 0L;
     private int hitCount = 0;
     private long dpsAvg = 0L;
     private final List<Removal<Integer>> dpsAvgDamageList = new ArrayList<>();
     private Instant tickHandle = Instant.now();
+    private boolean isInGreg = false;
+    private boolean gregInit = false;
+    private long invincibleTime = 0L; //it must be used only in tna (tick)
+    public static boolean debugMode = false;
     @SubscribeEvent
-    public void onRender(RenderGameOverlayEvent e){
+    public void onRender(RenderGameOverlayEvent e) throws IllegalAccessException {
         if(hud && e.getType() == RenderGameOverlayEvent.ElementType.ALL) render();
     }
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent e){
+        if(Minecraft.getMinecraft().world != null&& gregInit){
+            for(Entity en : Minecraft.getMinecraft().world.getEntities(EntityArmorStand.class, (ent) -> ent.getDisplayName().getFormattedText().equals("§c§9§lThe §1§k12345§9§l Anomaly§r"))) {
+                reset();
+                isInGreg = true;
+            }
+            if(!gregInit) return;
+        }
         if(tickHandle.toEpochMilli()+50 > Instant.now().toEpochMilli()) return;
         tickHandle = Instant.now();
         checkExpires();
@@ -49,7 +62,11 @@ public class EventListener {
             reset();
         }
         if(Minecraft.getMinecraft().world == null) return;
-        for(Entity en : Minecraft.getMinecraft().world.getEntities(EntityArmorStand.class, (ent) -> ent.getDisplayName().getUnformattedText().contains("-"))) {
+        for(Entity en : Minecraft.getMinecraft().world.getEntities(EntityArmorStand.class, (ent) -> (ent.getDisplayName().getUnformattedText().contains("-")||ent.getDisplayName().getFormattedText().equals("§e§4Sightseeing Interceptor§6 [Lv. 30]§r")))) {
+            if(isInGreg && en.getDisplayName().getFormattedText().equals("§e§4Sightseeing Interceptor§6 [Lv. 30]§r")){
+                invincibleTime++;
+                continue;
+            }
             String name = en.getDisplayName().getUnformattedText();
             name = name.replaceAll(colorReg, "");
             int c = 0;
@@ -68,8 +85,20 @@ public class EventListener {
             if (c != 0) addDamage(en.getUniqueID().toString(), c);
         }
     }
-    private void render(){
-        Minecraft.getMinecraft().fontRenderer.drawString("RealDPS: " + dpsAvg, ShowCommand.x, ShowCommand.y,0xffffff);
+    private void render() throws IllegalAccessException {
+        if(debugMode) {
+            List<String> txt = new ArrayList<>();
+            for (Field field : this.getClass().getDeclaredFields()) {
+                boolean isAccessible = field.isAccessible();
+                field.setAccessible(true);
+                if(!Modifier.isFinal(field.getModifiers())) txt.add(field.getName()+": "+field.get(this));
+                field.setAccessible(isAccessible);
+            }
+            int times = 0;
+            for(String tx : txt){
+                Minecraft.getMinecraft().fontRenderer.drawString(tx, ShowCommand.x, ShowCommand.y+Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT*times++,0xffffff);
+            }
+        }else Minecraft.getMinecraft().fontRenderer.drawString("RealDPS: " + dpsAvg, ShowCommand.x, ShowCommand.y,0xffffff);
     }
     private boolean isBossStartTitle(String a){
         return a.equals("The defense system") || a.equals("The Light Beast") || a.equals("The Grootslang Wyrmlings");
@@ -86,8 +115,9 @@ public class EventListener {
     public void onChatReceive(ClientChatReceivedEvent e) {
         String s = e.getMessage().getUnformattedText().replaceAll(colorReg, "");
         //the nameless anomaly boss prepare room lore.
-        if (s.contains("And what is responsible? What remains here should not- trapped far from whence it came. What feeds the paradox")) {
+        if (s.startsWith("And what is responsible? What remains here should not- trapped far from whence it came. What feeds the paradox")) {
             reset();
+            gregInit = true;
         } else if(s.equals("[1/1] The Mummyboard: OS Version M-37 is now online. Systems estimate a 96.286573628% chance of success.")) {
             reset();
         }else if(s.equals("[WAR] The battle has begun!")){
@@ -95,7 +125,12 @@ public class EventListener {
         } else if(s.startsWith("You finished the Legendary Challenge")){
             ShowCommand.showMsg(Minecraft.getMinecraft().player);
         } else if(s.contains("[!] The Raid Reward chest has been unlocked.")){
-            ShowCommand.showMsg(Minecraft.getMinecraft().player);
+            if(isInGreg) {
+                ShowCommand.showMsg(Minecraft.getMinecraft().player, invincibleTime);
+                isInGreg=false;
+                invincibleTime=0L;
+            }
+            else ShowCommand.showMsg(Minecraft.getMinecraft().player);
         } else if(s.equals("                        Territory Captured")){
             Thread t = new Thread(()->{
                 try {
@@ -134,7 +169,7 @@ public class EventListener {
         long i = Instant.now().toEpochMilli();
         List<String> toRemove = new ArrayList<>();
         dpsAvgDamageList.removeAll(dpsAvgDamageList.stream().filter(Removal::checkExpire).collect(Collectors.toList()));
-        dpsAvg= 0;
+        dpsAvg=0L;
         dpsAvgDamageList.forEach(a -> dpsAvg += a.b);
         for(Map.Entry<String, Instant> ep : expire.entrySet()){
             if(ep.getValue().toEpochMilli() + EXPIRE_WHEN <= i){
@@ -161,9 +196,12 @@ public class EventListener {
     public void reset(){
         expire.clear();
         damageList.clear();
-        damage = 0;
+        damage = 0L;
         hitCount = 0;
         start = Instant.now();
+        isInGreg = false;
+        gregInit = false;
+        invincibleTime = 0L;
     }
     public long getTotalDamage(){
         makeAllExpire();
--- src/main/java/com/github/soramame0256/showmemydps/ShowCommand.java
+++ src/main/java/com/github/soramame0256/showmemydps/ShowCommand.java
@@ -6,7 +6,10 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
+import javax.annotation.Nullable;
 import java.time.Instant;
+
+import static com.github.soramame0256.showmemydps.EventListener.debugMode;
 
 public class ShowCommand extends CommandBase {
     public static int x = 450;
@@ -33,6 +36,8 @@ public class ShowCommand extends CommandBase {
             if(args[0].equalsIgnoreCase("reset")){
                 showMsg(sender);
                 ShowMeMyDps.EVENT_LISTENER.reset();
+            }else if(args[0].equalsIgnoreCase("debug")){
+                sender.sendMessage(new TextComponentString("debugMode: " + (debugMode = !debugMode)));
             }
             if(args.length >= 2 && args[0].equalsIgnoreCase("hud")){
                 if(args.length == 2 && args[1].equalsIgnoreCase("toggle")){
@@ -55,6 +60,18 @@ public class ShowCommand extends CommandBase {
         long in = Instant.now().getEpochSecond() - instant.getEpochSecond();
         sender.sendMessage(new TextComponentString("§bDPS: " + totalDamage/in));
         sender.sendMessage(new TextComponentString("§b→Time: " + in + " seconds"));
+        sender.sendMessage(new TextComponentString("§bAvg/hit: " + totalDamage/hitCount));
+        sender.sendMessage(new TextComponentString("§bTotal: " + totalDamage));
+    }
+    public static void showMsg(ICommandSender sender,long invincibleTime){
+        long totalDamage = ShowMeMyDps.EVENT_LISTENER.getTotalDamage();
+        int hitCount = ShowMeMyDps.EVENT_LISTENER.getHitCount();
+        if(hitCount == 0) hitCount++;
+        Instant instant = ShowMeMyDps.EVENT_LISTENER.getStartWhen();
+        long in = Instant.now().getEpochSecond() - instant.getEpochSecond();
+        sender.sendMessage(new TextComponentString("§bDPS: " + totalDamage/(in-invincibleTime/20L)));
+        sender.sendMessage(new TextComponentString("§b→Time: " + (in-invincibleTime/20L) + " seconds"));
+        sender.sendMessage(new TextComponentString("§b→Actual: " + in + " seconds"));
         sender.sendMessage(new TextComponentString("§bAvg/hit: " + totalDamage/hitCount));
         sender.sendMessage(new TextComponentString("§bTotal: " + totalDamage));
     }
